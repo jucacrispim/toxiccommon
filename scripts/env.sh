3478dd44e259b401b3294a9edd562e8752c8857a
Juca Crispim <jucacrispim@gmail.com>
2023-06-11 20:17:24 -0300

    First commit for toxiccommon

---
+++ scripts/env.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker run -d -p zookeeper:latest
+docker run -d -p rabbitmq:latest
---
+++ tests/unit/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+import atexit
+
+from toxiccommon.coordination import ToxicZKClient  # noqa: F402
+
+
+def clean():
+    loop = asyncio.get_event_loop()
+    if ToxicZKClient._zk_client:
+        try:
+            loop.run_until_complete(ToxicZKClient._zk_client.close())
+        except Exception:
+            pass
+
+
+atexit.register(clean)
